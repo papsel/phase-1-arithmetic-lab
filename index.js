5852d842935b542e4abf270b9e553580b57c4ce8
papsel <elijah.njora@student.moringaschool.com>
2023-06-20 15:29:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+function multiply(){
+    const num1 = 31;
+    const num2 = 2;
+    return num1 * num2
+}
+const result =multiply();
+console.log(result)
 
-// Write your code here
+function random(){
+    return Math.floor(Math.random()* Number.MAX_SAFE_INTEGER) + 1;
+}
+const randomNumber = random()
+console.log(randomNumber);
+
+function mod(){
+    return num3 % num4
+    
+}
+const num3 = 19
+const num4 = 5;
+const remainder = mod(num3,num4)
+console.log(remainder)
+
+function max(numbers){
+    return Math.max(...numbers)
+}
+const numbersSet= [11,8,6,3,20];
+const highestNumber =max(numbersSet);
+console.log(highestNumber);
